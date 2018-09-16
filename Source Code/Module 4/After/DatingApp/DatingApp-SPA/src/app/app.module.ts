7a72cfe7cd83177d8858e2542429d0219782b1ea
Jini Byun <jinibyun@gmail.com>
2018-09-16 00:39:41 -0400

    commit

--- Source Code/Module 4/After/DatingApp/DatingApp-SPA/src/app/app.module.ts
+++ Source Code/Module 4/After/DatingApp/DatingApp-SPA/src/app/app.module.ts
@@ -1,10 +1,14 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import {HttpClientModule} from '@angular/common/http';
+
+// to use angular form
 import {FormsModule} from '@angular/forms';
 
 import { AppComponent } from './app.component';
 import { NavComponent } from './nav/nav.component';
+
+// service
 import { AuthService } from './_services/auth.service';
 import { HomeComponent } from './home/home.component';
 import { RegisterComponent } from './register/register.component';
--- Source Code/Module 4/After/DatingApp/DatingApp-SPA/src/app/nav/nav.component.ts
+++ Source Code/Module 4/After/DatingApp/DatingApp-SPA/src/app/nav/nav.component.ts
@@ -24,7 +24,7 @@ export class NavComponent implements OnInit {
 
   loggedIn() {
     const token = localStorage.getItem('token');
-    return !!token;
+    return !!token; // !! means retrun true of false
   }
 
   logout() {
